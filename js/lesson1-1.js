899a158afe42f6c2f5aa8631794e8012e847cc75
Will IM <vitalii.mieliet@gmail.com>
2025-04-03 15:07:41 +0300

    blabla

--- js/lesson1-1.js
+++ js/lesson1-1.js
@@ -1,6 +1,27 @@
-console.log("Hello guys!!! 9 чи 10?");
-console.log(10);
-console.log("10");
-console.log("9");
+// console.log("Hello guys!!! 9 or 10?");
+// console.log(10);
+// console.log("10");
+// console.log("9");
+
 const userName = "Fulhensio";
-console.log(userName);
+const userAge = 25;
+const isDeveloper = false;
+
+// console.log(typeof userName);
+// console.log(typeof userAge);
+// console.log(typeof isDeveloper);
+
+// console.log(typeof true);
+// console.log(typeof "true");
+
+// console.log(typeof typeof 1);
+
+// console.log(typeof NaN);
+// console.log(typeof null);
+// console.log(typeof undefined);
+
+// alert("Hello");
+// const answer = prompt("Enter your answer:");
+// console.log(answer);
+// const confirmation = confirm("Are you adult?");
+// console.log(confirmation);
