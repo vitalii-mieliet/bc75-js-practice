c7e1e43648097e9c610274e880fc14fcdb1bf7ce
Anna Taranova <annsheremet123@gmail.com>
2025-04-04 14:17:06 +0200

    praktice

--- js/lesson1-2.js
+++ js/lesson1-2.js
@@ -24,3 +24,45 @@
 // console.log(undefined + 1); 
 // console.log("" - 1); 
 // console.log("" * 2);
+
+
+// console.log(Math.ceil(1.3)); 
+// console.log(Math.floor(1.7)); 
+// console.log(Math.round(1.5)); 
+// console.log(Math.max(3, 7, 2)); 
+// console.log(Math.min(3, 7, 2)); 
+// console.log(Math.pow(2, 3)); 
+// console.log(Math.sqrt(25)); 
+// console.log(Math.random());
+
+// function getRandomInt(min, max) {
+//   return Math.floor(Math.random() * (max - min + 1)) + min;
+// }
+// console.log(getRandomInt(5, 10));
+
+// console.log(5 > 4);
+// console.log(10 >= "7");
+// console.log("2" > "12");
+// console.log("2" < "12");
+// console.log("4" == 4);
+// console.log("6" === 6);
+// console.log("false" === false);
+// console.log(1 == true);
+// console.log(1 === true);
+// console.log("0" == false);
+// console.log("0" === false);
+// console.log("a" > "A");
+// console.log("Papaya" < "papaya");
+// console.log("Papaya" === "papaya");
+// console.log(undefined == null);
+// console.log(undefined === null);
+// console.log(NaN == NaN);
+// console.log(NaN === NaN);
+// console.log (Number.isNaN(NaN))
+
+// // Метод charCodeAt(index) повертає числовий код символу (unicode) за вказаним індексом у рядку.
+// // string.charCodeAt(index)
+// // index — позиція символу (нумерація починається з 0).
+// // Якщо index виходить за межі рядка, повертає NaN.
+// console.log ("a".charCodeAt(0))
+// console.log ("A".charCodeAt(0))
