d1f8b6b4d440635767dea017fcd4a88382eacb48
Stanislav <17samsonov@gmail.com>
2025-04-04 14:37:55 +0300

    lesson2-s

---
+++ js/lesson1-2.js
@@ -0,0 +1,26 @@
+// console.log(Number("5"));
+// console.log(Number(true)); 
+// console.log(Number(false)); 
+// console.log(Number(null)); 
+// console.log(Number(undefined));
+// console.log(Number("Jacob")); 
+// console.log(Number("25px"));
+// console.log(String(5)); 
+// console.log(String(true)); 
+// console.log(String(false));
+// console.log(String(null)); //
+// console.log(String(undefined)); 
+// console.log(Boolean(undefined));
+// console.log(Boolean(0));
+// console.log(Boolean("0"));
+
+// console.log("5" * 2); 
+// console.log("10" - 5); 
+// console.log(5 + true); 
+// console.log(5 - true); 
+// console.log("5" + true); 
+// console.log(Number("5") + 1); 
+// console.log(null + 1); 
+// console.log(undefined + 1); 
+// console.log("" - 1); 
+// console.log("" * 2);
