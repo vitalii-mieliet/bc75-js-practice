a78cf079cbb2260114e7d7f792387a7300884cbb
Maryna <Milanataran910@gmail.com>
2025-04-08 14:01:15 +0200

    add js file

---
+++ js/lesson2-2.js
@@ -0,0 +1,39 @@
+// const message = "  This is string's methods!";
+// const fullCopyMessage = message.slice();
+// console.log(fullCopyMessage);
+// const partCopyMessage = message.slice(0, 11);
+// console.log(partCopyMessage + "...");
+
+// const lowerMessage = message.toLowerCase();
+// const upperMessage = message.toUpperCase();
+// console.log(lowerMessage);
+// console.log(upperMessage);
+
+// console.log(message.includes("s"));
+// console.log(message.includes("S"));
+
+// console.log(message.startsWith("t"));
+// console.log(message.startsWith("T"));
+
+// console.log(message.endsWith("!"));
+
+// const index = message.indexOf("I");
+// console.log(index);
+
+// const trimMessage = message.trim();
+// console.log(trimMessage);
+
+//   Напиши функцію, яка приймає адресу сайта url та перевіряє чи закінчується значення
+//   параметра символом /. Якщо ні, додай до кінця
+//   значення url цей символ і поверни оновлене значення з функції, але тільки в тому випадку,
+//   якщо в url є підрядок "my-site".
+// В іншому випадку поверни з функції адресу в незмінному вигляді
+
+// const url = "https://my-site.com/about";
+// function checkUrl(url) {
+//   if (!url.endsWith("/") && url.includes("my-site")) {
+//     return `${url}/`;
+//   }
+//   return url;
+// }
+// console.log(checkUrl(url));
