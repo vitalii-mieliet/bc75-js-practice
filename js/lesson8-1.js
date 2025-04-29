61945a57d5be6f0aece51a0fb0d411a455a008bc
Danya76131 <danylo.galetskiy@gmail.com>
2025-04-29 14:34:04 +0200

    library

--- js/lesson8-1.js
+++ js/lesson8-1.js
@@ -5,114 +5,124 @@
 // При кліку на картку:
 // - Додавай або прибирай клас active на картці.
 // - Виводь повну інформацію про обраний елемент у окремий <div class="details"></div>.
-// const tweets = [
-//   {
-//     id: "000",
-//     name: "Alex",
-//     gender: "male",
-//     likes: 5,
-//     tags: ["js", "nodejs"],
-//     hobbies: ["cycling", "gaming"],
-//     about: "Full-stack розробник, люблю велосипеди та ігри.",
-//   },
-//   {
-//     id: "001",
-//     name: "Kate",
-//     gender: "female",
-//     likes: 2,
-//     tags: ["html", "css"],
-//     hobbies: ["drawing", "travelling"],
-//     about: "Фронтенд ентузіаст, обожнюю мистецтво і подорожі.",
-//   },
-//   {
-//     id: "002",
-//     name: "Maria",
-//     gender: "female",
-//     likes: 17,
-//     tags: ["html", "js", "nodejs"],
-//     hobbies: ["reading", "cooking"],
-//     about: "Постійно вчуся новому. Люблю книги й кулінарні експерименти.",
-//   },
-//   {
-//     id: "003",
-//     name: "Borys",
-//     gender: "male",
-//     likes: 8,
-//     tags: ["css", "react"],
-//     hobbies: ["football", "photography"],
-//     about: "Верстальник і майбутній React-розробник. Фанат футболу.",
-//   },
-//   {
-//     id: "004",
-//     name: "Jhon",
-//     gender: "male",
-//     likes: 10,
-//     tags: ["js", "nodejs", "react"],
-//     hobbies: ["hiking", "tech reviews"],
-//     about: "Мандрівник у душі та оглядач техно-новинок.",
-//   },
-//   {
-//     id: "005",
-//     name: "Anna",
-//     gender: "female",
-//     likes: 3,
-//     tags: ["js", "nodejs", "react"],
-//     hobbies: ["yoga", "baking"],
-//     about: "Йога, випічка та розробка — три мої любові.",
-//   },
-//   {
-//     id: "006",
-//     name: "Jhon",
-//     gender: "male",
-//     likes: 0,
-//     tags: ["js", "nodejs", "react"],
-//     hobbies: ["chess", "gaming"],
-//     about: "Геймер і шахіст. Кодинг — це моя нова гра.",
-//   },
-// ];
+const tweets = [
+  {
+    id: "000",
+    name: "Alex",
+    gender: "male",
+    likes: 5,
+    tags: ["js", "nodejs"],
+    hobbies: ["cycling", "gaming"],
+    about: "Full-stack розробник, люблю велосипеди та ігри.",
+  },
+  {
+    id: "001",
+    name: "Kate",
+    gender: "female",
+    likes: 2,
+    tags: ["html", "css"],
+    hobbies: ["drawing", "travelling"],
+    about: "Фронтенд ентузіаст, обожнюю мистецтво і подорожі.",
+  },
+  {
+    id: "002",
+    name: "Maria",
+    gender: "female",
+    likes: 17,
+    tags: ["html", "js", "nodejs"],
+    hobbies: ["reading", "cooking"],
+    about: "Постійно вчуся новому. Люблю книги й кулінарні експерименти.",
+  },
+  {
+    id: "003",
+    name: "Borys",
+    gender: "male",
+    likes: 8,
+    tags: ["css", "react"],
+    hobbies: ["football", "photography"],
+    about: "Верстальник і майбутній React-розробник. Фанат футболу.",
+  },
+  {
+    id: "004",
+    name: "Jhon",
+    gender: "male",
+    likes: 10,
+    tags: ["js", "nodejs", "react"],
+    hobbies: ["hiking", "tech reviews"],
+    about: "Мандрівник у душі та оглядач техно-новинок.",
+  },
+  {
+    id: "005",
+    name: "Anna",
+    gender: "female",
+    likes: 3,
+    tags: ["js", "nodejs", "react"],
+    hobbies: ["yoga", "baking"],
+    about: "Йога, випічка та розробка — три мої любові.",
+  },
+  {
+    id: "006",
+    name: "Jhon",
+    gender: "male",
+    likes: 0,
+    tags: ["js", "nodejs", "react"],
+    hobbies: ["chess", "gaming"],
+    about: "Геймер і шахіст. Кодинг — це моя нова гра.",
+  },
+];
 
-// function createMarkup(arr) {
-//   return arr
-//     .map(({ id, name, tags, likes, gender }) => {
-//       return `<li class="stats-item ${
-//         gender === "male" ? "male" : "female" // we can skip
-//       }" id=${id}>
-//         <p class="stats-name">${name}</p>
-//         <p class="stats-likes">${likes} likes</p>
-//         <p class="stats-tags">tags: ${tags}</p>
-//       </li>`;
-//     })
-//     .join("");
-// }
+function createMarkup(arr) {
+  return arr
+    .map(({ id, name, tags, likes, gender }) => {
+      return `<li class="stats-item ${
+        gender === "male" ? "male" : "female" // we can skip
+      }" id=${id}>
+        <p class="stats-name">${name}</p>
+        <p class="stats-likes">${likes} likes</p>
+        <p class="stats-tags">tags: ${tags}</p>
+      </li>`;
+    })
+    .join("");
+}
 
-// const list = document.querySelector(".stats");
-// const details = document.querySelector(".details");
+const list = document.querySelector(".stats");
+const details = document.querySelector(".details");
 
-// list.insertAdjacentHTML("beforeend", createMarkup(tweets));
+list.insertAdjacentHTML("beforeend", createMarkup(tweets));
 
-// list.addEventListener("click", getInfo);
+tippy('.stats li', {
+  // content: 'Click on me!',
+  content(reference) {
+    return `Click on ${reference.firstElementChild.textContent}`
+  },
+    trigger: 'click',
+    theme: 'space'
+      });
 
-// function getInfo(e) {
-//   const li = e.target.closest("li");
 
-//   if (!li) {
-//     return;
-//   }
-//   const activeItem = list.querySelector(".stats-item.active")
-//   if (activeItem) {
-//     activeItem.classList.remove("active")
-//   }
-//    li.classList.add("active")
-//   const tweet = tweets.find((item) => item.id === li.id);
-//   const markup = `
-//         <h2>${tweet.name}</h2>
-//         <p><strong>Likes:</strong> ${tweet.likes}</p>
-//         <p><strong>Tags:</strong> ${tweet.tags.join(", ")}</p>
-//         <p><strong>Hobbies:</strong> ${tweet.hobbies}</p>
-//         <p><strong>About:</strong> ${tweet.about}</p>
-//       `
-//   details.innerHTML = markup;
-// }
+list.addEventListener("click", getInfo);
+
+function getInfo(e) {
+  const li = e.target.closest("li");
+
+  if (!li) {
+    return;
+  }
+  const activeItem = list.querySelector(".stats-item.active")
+  if (activeItem) {
+    activeItem.classList.remove("active")
+  }
+   li.classList.add("active")
+  const tweet = tweets.find((item) => item.id === li.id);
+  const markup = `
+        <h2>${tweet.name}</h2>
+        <p><strong>Likes:</strong> ${tweet.likes}</p>
+        <p><strong>Tags:</strong> ${tweet.tags.join(", ")}</p>
+        <p><strong>Hobbies:</strong> ${tweet.hobbies}</p>
+        <p><strong>About:</strong> ${tweet.about}</p>
+      `
+  details.innerHTML = markup;
+}
 
 
 // 9. При натисканні на кожну з кнопок підсумовуються значення з data-атрибутів.
